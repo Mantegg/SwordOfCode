46a7b45aefdcee55c3dd0a8b5f2d9247003b3714
Mantegg <aimansuaimi793@gmail.com>
2024-03-04 22:56:44 +0800

    COMPILER DONE
    -fix loop on timer

--- UI/CodingSpace.gd
+++ UI/CodingSpace.gd
@@ -1,15 +1,15 @@
 extends Control
 
 @onready var lblOutput = $HSplitContainer/TabContainer/Output/VBoxContainer/lblOutput
+var token
 
 func _on_btn_run_pressed():
 	var javaCode = $HSplitContainer/ColorRect2/Code.text
-	var encodedJavaCode = javaCode.uri_encode()
+	var encodedJavaCode = Marshalls.utf8_to_base64(javaCode)
 	#print(javaCode)
 	#print(encodedJavaCode)
 	$HTTPRequestInput.cancel_request()
 	$HTTPRequestInput.request("http://localhost/SwordOfCode/createSubmission.php?sourceCode=" + encodedJavaCode)
-	
 	
 	$HSplitContainer/TabContainer.current_tab = 1
 
@@ -18,15 +18,20 @@ func _on_btn_reset_pressed():
 
 func _on_http_request_request_completed(result, response_code, headers, body):
 	var output = JSON.parse_string(body.get_string_from_utf8())
-	print(output["token"])
-	$HTTPRequestOutput.cancel_request()
-	$HTTPRequestOutput.request("http://localhost/SwordOfCode/getSubmission.php?token="+output["token"])
-	
+	token = output["token"]
+	#print(token)
+	$Timer.start()
 	$HTTPRequestInput.cancel_request()
 
 func _on_http_request_output_request_completed(result, response_code, headers, body):
 	var output = JSON.parse_string(body.get_string_from_utf8())
-	print(output)
-	
+	var outputStr = Marshalls.base64_to_utf8(output["stdout"])
+	lblOutput.text = outputStr
 	$HTTPRequestOutput.cancel_request()
 
+func _on_timer_timeout():
+	$HTTPRequestOutput.cancel_request()
+	$HTTPRequestOutput.request("http://localhost/SwordOfCode/getSubmission.php?token="+token)
+	#$HTTPRequestOutput.request("http://localhost/SwordOfCode/getSubmission.php?token=70d368a2-542c-4b76-b239-536ada6c7221")
+	
+	
